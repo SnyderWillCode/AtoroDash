b3e07a69169aa7412b605f5b7d94906a8afaf315
NaysKutzu <ghermancassian2008@gmail.com>
2024-12-25 21:58:21 +0100

    PUSH -> Lint fix

--- frontend/src/components/client/Dashboard/Account/Security.vue
+++ frontend/src/components/client/Dashboard/Account/Security.vue
@@ -60,7 +60,7 @@ const disable2FA = () => {
         :cardDescription="t('account.pages.security.page.cards.twofactor.subTitle')"
     >
         <div v-if="is2FAEnabled" class="flex items-center justify-between">
-            <p class="text-sm text-gray-100">{{ t('account.pages.security.page.cards.twofactor.disable_button.description')}}<</p>
+            <p class="text-sm text-gray-100">{{ t('account.pages.security.page.cards.twofactor.disable_button.description')}}</p>
             <button
                 @click="disable2FA"
                 class="ml-4 px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg text-sm font-medium transition-colors"

a57e66c35e80aad1769c1f6db964f4ca85c8f102
NaysKutzu <ghermancassian2008@gmail.com>
2024-12-26 01:08:40 +0100

    PUSH -> Fat DB

---
+++ backend/storage/25.12.2024-22.03-create-tickets-attachments.sql
@@ -0,0 +1,12 @@
+CREATE TABLE
+    `mythicalclient_tickets_attachments` (
+        `id` INT NOT NULL AUTO_INCREMENT,
+        `ticket` int(16) NOT NULL,
+        
+        `enabled` ENUM ('false', 'true') NOT NULL DEFAULT 'false',
+        `deleted` ENUM ('false', 'true') NOT NULL DEFAULT 'false',
+        `locked` ENUM ('false', 'true') NOT NULL DEFAULT 'false',
+        `date` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (`id`),
+        FOREIGN KEY (`ticket`) REFERENCES `mythicalclient_tickets` (`id`)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
